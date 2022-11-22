ce21596db74029b9247c75c536445b95810f9484
rBLOT-Arcenz <r.blot@protonmail.com>
2022-11-22 09:13:43 +0100

    MAIN PROJ

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    Camera cam;
+    CharacterController characterController;
+    float maxSpeed = 10, acceleration = 10, jumpForce = 5;
+    float speed, verticalMovement;
+    Vector3 direction, directionForward, directionRight, nextDir;
+    Animator animator;
+    [SerializeField]
+    AudioClip stepSound;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        cam = Camera.main;
+        direction = transform.forward;
+        nextDir = transform.forward;
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        gravity();
+
+        Move();
+
+
+        //apply the calculated movement to the character controller movement system
+        characterController.Move((direction * speed + verticalMovement * Vector3.up) * Time.deltaTime);
+
+        animator.SetFloat("Speed", speed / maxSpeed);
+    }
+
+    private void Move()
+    {
+        if ((Input.GetAxisRaw("Vertical")) != 0 || (Input.GetAxisRaw("Horizontal")) != 0)
+        {
+            //gets the inputs from keyboard arrows and defines the direction depending on the camera's orientation;
+
+            directionForward = cam.transform.forward;
+            directionForward.y = 0;
+            directionForward *= Input.GetAxisRaw("Vertical");
+
+            directionRight = cam.transform.right;
+            directionRight.y = 0;
+            directionRight *= Input.GetAxisRaw("Horizontal");
+
+            nextDir = Vector3.Normalize(directionForward + directionRight);
+
+            //Direction interpolation between the current direction and the inputed direction
+            direction = Vector3.Lerp(direction, nextDir, Time.deltaTime * 2);
+
+            //Calculate the speed increasement depending on the time spent pushing an arrow button;
+
+            if (speed < maxSpeed)
+            {
+                speed += acceleration * Time.deltaTime;
+            }
+            else
+            {
+                speed = maxSpeed;
+            }
+
+        }
+        else
+        {
+            //Calculate the speed decreasement depending on the time since no arrow button is pressed;
+
+            if (speed != 0)
+            {
+                if (speed <= 2 * acceleration * Time.deltaTime)
+                    speed = 0;
+                else
+                {
+                    speed -= 2 * acceleration * Time.deltaTime;
+                }
+            }
+        }
+
+        //make the object rotate toward its movement;
+        transform.rotation = Quaternion.LookRotation(direction, transform.up);
+    }
+
+    private void gravity()
+    {
+        if (verticalMovement <= 0 && characterController.isGrounded)
+        {
+            verticalMovement = -5;
+        }
+        else
+        {
+            verticalMovement -= jumpForce * 2 * Time.deltaTime;
+        }
+    }
+
+    // Fonction appelée lors de chaque pas grâce à un animation event intégré dans le cycle de marche du personnage
+    public void StepSound()
+    {
+        // À remplacer lorsque vous intégrerez les sons de pas
+        if (stepSound != null)
+        {
+            GetComponent<AudioSource>().PlayOneShot(stepSound);
+        }
+        else
+        {
+            Debug.Log("Il faut intégrer l'audioclip dans le script Character !!!");
+        }
+
+
+    }
+}
---
+++ Assets/Script/DoorScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Assignation de son propre animator en tant que variable pour pouvoir y accéder plus simplement
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    //déclence l'animation d'ouverture des portes
+    //Y intégrer le jeu d'un son ? Le lancement d'une corroutine ?
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetBool("In", true);
+    }
+
+    //déclence l'animation de fermeture des portes
+    //Y intégrer le jeu d'un son ? Le lancement d'une corroutine ?
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("In", false);
+    }
+
+    //Créer une fonction publique à appeler lors d'un animation event ?
+
+}
